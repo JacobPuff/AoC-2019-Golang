51f0b03a79b52a009f496aca414d41ced74e99e6
JacobPuff <jacob@todandlorna.com>
2019-12-03 14:28:10 -0700

    Merge day1 into main file, use kingpin to get day

--- main.go
+++ main.go
@@ -1,1 +1,162 @@
 // this will eventually just be the main package, and it will run all the things here
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	day = kingpin.Arg("day", "Advent of code day to run").Int()
+)
+
+func main() {
+	kingpin.Version("0.0.1")
+	kingpin.Parse()
+	fmt.Printf("Would run day %d\n", *day)
+	switch *day {
+	case 1:
+		day1()
+	default:
+		fmt.Println("We don't have that day...")
+	}
+}
+
+func getFuelForFuel(initialFuelAmmount int) (total int) {
+	nextAmount := initialFuelAmmount
+	for nextAmount > 0 {
+		nextAmount = (nextAmount / 3) - 2
+		if nextAmount > 0 {
+			total += nextAmount
+		}
+	}
+	return total
+
+}
+
+func day1() {
+	//data is at the bottom of the page
+	total := 0
+	dataArray := strings.Split(data, "\n")
+	for _, item := range dataArray {
+		fuelValue, err := strconv.Atoi(item)
+		if err != nil {
+			fmt.Println(err)
+		}
+		total += (fuelValue / 3) - 2
+	}
+	fmt.Println("Fuel for modules: ", strconv.Itoa(total))
+	for _, item := range dataArray {
+		moduleMass, _ := strconv.Atoi(item)
+		fuelMass := (moduleMass / 3) - 2
+		if fuelMass > 0 {
+			total += getFuelForFuel(fuelMass)
+		}
+	}
+	fmt.Println("Fuel for modules with the fuel for fuel: ", strconv.Itoa(total))
+
+}
+
+const data = `73617
+104372
+131825
+85022
+105514
+78478
+87420
+118553
+97680
+89479
+146989
+79746
+108085
+117895
+143811
+102509
+102382
+92975
+72978
+94208
+130521
+83042
+138605
+107566
+63374
+71176
+129487
+118408
+115425
+63967
+98282
+121829
+92834
+61084
+70122
+87221
+132773
+141347
+133225
+81199
+94994
+60881
+110074
+63499
+143107
+76618
+86818
+135394
+106908
+96085
+99801
+112903
+51751
+56002
+70924
+62180
+133025
+68025
+122660
+64898
+77339
+62109
+133891
+134460
+84224
+54836
+59748
+125540
+67796
+71845
+92899
+130103
+74612
+136820
+96212
+132002
+97405
+82629
+63717
+62805
+112693
+147810
+139827
+116220
+69711
+50236
+137833
+103743
+147456
+112098
+84867
+75615
+132738
+81072
+89444
+58443
+94465
+112494
+82127
+132533`
