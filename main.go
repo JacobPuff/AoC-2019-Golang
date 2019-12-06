bfc22c6e9145f7e29745e6dcef631f39c90760ab
JacobPuff <jacob@todandlorna.com>
2019-12-05 21:55:16 -0700

    Preparations for day 6

--- main.go
+++ main.go
@@ -29,6 +29,8 @@ func main() {
 		day4()
 	case 5:
 		day5()
+	case 6:
+		day6()
 	default:
 		fmt.Println("We don't have that day...")
 	}
@@ -75,8 +77,22 @@ func getParamWithMode(data []int64, mode int64, location int64) (paramVal int64)
 		return data[data[location]]
 	case 1:
 		return data[location]
+	default:
+		fmt.Printf("No mode with value %d when getting location %d", mode, location)
+		return -1
 	}
-	fmt.Println("No mode with that value")
+}
+
+func setParamWithMode(data []int64, mode int64, location int64, value int64) (paramVal int64) {
+	switch mode {
+	case 0:
+		data[data[location]] = value
+	case 1:
+		data[location] = value
+	default:
+		fmt.Printf("Cant set with mode of value %d when setting location %d", mode, location)
+		return -1
+	}
 	return 0
 }
 
@@ -86,41 +102,35 @@ func getResult(intCodeData []int64) (atZero int64) {
 	copy(copiedData, intCodeData)
 	var currentPos int64 = 0
 	instruction := copiedData[currentPos]
+	running := true
 
 	var firstVal int64
 	var secondVal int64
-	var placePos int64
-
-	running := true
-	//inputReader := bufio.NewReader(os.Stdin)
 
 	for running {
 		currentOpCode := instruction % 100
-		fmt.Println(currentOpCode)
 		mode1 := (instruction / 100) % 10
 		mode2 := (instruction / 1000) % 10
 		mode3 := (instruction / 10000) % 10
-		fmt.Println(mode1, " ", mode2, " ", mode3)
+		var result int64
 
-		if mode3 == 0 {
-			placePos = copiedData[currentPos+3]
-		} else {
-			placePos = currentPos + 3
-		}
-
-		//Run things for op codes
 		if currentOpCode == 1 {
 			firstVal = getParamWithMode(copiedData, mode1, currentPos+1)
 			secondVal = getParamWithMode(copiedData, mode2, currentPos+2)
-			copiedData[placePos] = firstVal + secondVal
-			fmt.Println("f: ", firstVal, " s: ", secondVal, "p: ", placePos)
+			result = setParamWithMode(copiedData, mode3, currentPos+3, firstVal+secondVal)
+			if result == -1 {
+				running = false
+			}
 			currentPos += 4
 		}
 
 		if currentOpCode == 2 {
 			firstVal = getParamWithMode(copiedData, mode1, currentPos+1)
 			secondVal = getParamWithMode(copiedData, mode2, currentPos+2)
-			copiedData[placePos] = firstVal * secondVal
+			result = setParamWithMode(copiedData, mode3, currentPos+3, firstVal*secondVal)
+			if result == -1 {
+				running = false
+			}
 			currentPos += 4
 		}
 
@@ -129,9 +139,10 @@ func getResult(intCodeData []int64) (atZero int64) {
 			var input int64
 			_, _ = fmt.Scanf("%d", &input)
 			// TODO: Stop people from inputing non-Numbers
-			fmt.Println(copiedData[copiedData[currentOpCode+1]])
-			copiedData[copiedData[currentOpCode+1]] = input
-			fmt.Println(copiedData[copiedData[currentOpCode+1]])
+			result = setParamWithMode(copiedData, mode1, currentPos+1, input)
+			if result == -1 {
+				running = false
+			}
 			fmt.Println("input: ", input)
 			currentPos += 2
 		}
@@ -145,6 +156,56 @@ func getResult(intCodeData []int64) (atZero int64) {
 			}
 			fmt.Println("Output: ", output)
 			currentPos += 2
+		}
+
+		if currentOpCode == 5 {
+			firstVal = getParamWithMode(copiedData, mode1, currentPos+1)
+			secondVal = getParamWithMode(copiedData, mode2, currentPos+2)
+			if firstVal != 0 {
+				currentPos = secondVal
+			} else {
+				currentPos += 3
+			}
+		}
+
+		if currentOpCode == 6 {
+			firstVal = getParamWithMode(copiedData, mode1, currentPos+1)
+			secondVal = getParamWithMode(copiedData, mode2, currentPos+2)
+			if firstVal == 0 {
+				currentPos = secondVal
+			} else {
+				currentPos += 3
+			}
+		}
+
+		if currentOpCode == 7 {
+			firstVal = getParamWithMode(copiedData, mode1, currentPos+1)
+			secondVal = getParamWithMode(copiedData, mode2, currentPos+2)
+			if firstVal < secondVal {
+				result = setParamWithMode(copiedData, mode3, currentPos+3, 1)
+			} else {
+				result = setParamWithMode(copiedData, mode3, currentPos+3, 0)
+			}
+
+			if result == -1 {
+				running = false
+			}
+			currentPos += 4
+		}
+
+		if currentOpCode == 8 {
+			firstVal = getParamWithMode(copiedData, mode1, currentPos+1)
+			secondVal = getParamWithMode(copiedData, mode2, currentPos+2)
+			if firstVal == secondVal {
+				result = setParamWithMode(copiedData, mode3, currentPos+3, 1)
+			} else {
+				result = setParamWithMode(copiedData, mode3, currentPos+3, 0)
+			}
+
+			if result == -1 {
+				running = false
+			}
+			currentPos += 4
 		}
 
 		if currentOpCode == 99 {
@@ -336,7 +397,15 @@ func day4() {
 
 func day5() {
 	inputData := []int64{3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 1101, 37, 61, 225, 101, 34, 121, 224, 1001, 224, -49, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 6, 224, 1, 224, 223, 223, 1101, 67, 29, 225, 1, 14, 65, 224, 101, -124, 224, 224, 4, 224, 1002, 223, 8, 223, 101, 5, 224, 224, 1, 224, 223, 223, 1102, 63, 20, 225, 1102, 27, 15, 225, 1102, 18, 79, 224, 101, -1422, 224, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 1, 224, 1, 223, 224, 223, 1102, 20, 44, 225, 1001, 69, 5, 224, 101, -32, 224, 224, 4, 224, 1002, 223, 8, 223, 101, 1, 224, 224, 1, 223, 224, 223, 1102, 15, 10, 225, 1101, 6, 70, 225, 102, 86, 40, 224, 101, -2494, 224, 224, 4, 224, 1002, 223, 8, 223, 101, 6, 224, 224, 1, 223, 224, 223, 1102, 25, 15, 225, 1101, 40, 67, 224, 1001, 224, -107, 224, 4, 224, 102, 8, 223, 223, 101, 1, 224, 224, 1, 223, 224, 223, 2, 126, 95, 224, 101, -1400, 224, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 3, 224, 1, 223, 224, 223, 1002, 151, 84, 224, 101, -2100, 224, 224, 4, 224, 102, 8, 223, 223, 101, 6, 224, 224, 1, 224, 223, 223, 4, 223, 99, 0, 0, 0, 677, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1105, 0, 99999, 1105, 227, 247, 1105, 1, 99999, 1005, 227, 99999, 1005, 0, 256, 1105, 1, 99999, 1106, 227, 99999, 1106, 0, 265, 1105, 1, 99999, 1006, 0, 99999, 1006, 227, 274, 1105, 1, 99999, 1105, 1, 280, 1105, 1, 99999, 1, 225, 225, 225, 1101, 294, 0, 0, 105, 1, 0, 1105, 1, 99999, 1106, 0, 300, 1105, 1, 99999, 1, 225, 225, 225, 1101, 314, 0, 0, 106, 0, 0, 1105, 1, 99999, 108, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 329, 101, 1, 223, 223, 1107, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 344, 101, 1, 223, 223, 8, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 359, 101, 1, 223, 223, 1008, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 374, 101, 1, 223, 223, 7, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 389, 1001, 223, 1, 223, 1007, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 404, 1001, 223, 1, 223, 7, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 419, 1001, 223, 1, 223, 1008, 677, 226, 224, 1002, 223, 2, 223, 1005, 224, 434, 1001, 223, 1, 223, 1107, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 449, 1001, 223, 1, 223, 1008, 226, 226, 224, 1002, 223, 2, 223, 1006, 224, 464, 1001, 223, 1, 223, 1108, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 479, 101, 1, 223, 223, 1108, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 494, 1001, 223, 1, 223, 107, 226, 226, 224, 1002, 223, 2, 223, 1006, 224, 509, 1001, 223, 1, 223, 8, 226, 677, 224, 102, 2, 223, 223, 1006, 224, 524, 1001, 223, 1, 223, 1007, 226, 226, 224, 1002, 223, 2, 223, 1006, 224, 539, 1001, 223, 1, 223, 107, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 554, 1001, 223, 1, 223, 1107, 226, 226, 224, 102, 2, 223, 223, 1005, 224, 569, 101, 1, 223, 223, 1108, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 584, 1001, 223, 1, 223, 1007, 677, 226, 224, 1002, 223, 2, 223, 1005, 224, 599, 101, 1, 223, 223, 107, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 614, 1001, 223, 1, 223, 108, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 629, 101, 1, 223, 223, 7, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 644, 101, 1, 223, 223, 8, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 659, 1001, 223, 1, 223, 108, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 674, 1001, 223, 1, 223, 4, 223, 99, 226}
+	// Test data. Prints 999 if input below 8, 1000 if equal, 1001 if greater.
+	//inputData := []int64{3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99}
+	// Test data. if input not 0 it prints 1, if input 0 it prints 0
+	//inputData := []int64{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9}
 	getResult(inputData)
+}
+
+func day6() {
+
 }
 
 const day1Data = `73617
