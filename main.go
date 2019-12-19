8dd6084acaf121e92d6750dddc5db3d9a39f9ab6
JacobPuff <jacob@todandlorna.com>
2019-12-19 00:19:46 -0700

    Finish day 19. Also add my incomplete day 18 stuff.

--- main.go
+++ main.go
@@ -62,6 +62,10 @@ func main() {
 		day16()
 	case 17:
 		day17()
+	case 18:
+		day18()
+	case 19:
+		day19()
 	default:
 		fmt.Println("We don't have that day...")
 	}
@@ -1879,6 +1883,195 @@ func day17() {
 	fmt.Println("\nAlign parameter sum:", alignParamSum)
 	scaffoldBotProgram[0] = 2
 	intComp(scaffoldBotProgram, in, out)
+}
+
+type Route struct {
+	pos        Point
+	length     int
+	prevPoints map[string]bool
+}
+
+func day18() {
+	file, err := os.Open("day18Tunnels.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	//Yoinking the droidTile struct because it has what I need
+	var mazeMap = make(map[Point]droidTile)
+	var doors = make(map[string]bool)
+	var keys = make(map[string]bool)
+	var x, y int64
+	var currentPos Point
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		for index := range line {
+			if line[index] == '@' {
+				// mazeMap[Point{x - 1, y}] = droidTile{"#", false}
+				// mazeMap[Point{x + 1, y}] = droidTile{"#", false}
+				currentPos = Point{x, y}
+			}
+			if line[index] != '#' && line[index] != '@' && line[index] != '.' {
+				stringChar := string(line[index])
+				if stringChar == strings.ToUpper(stringChar) {
+					doors[stringChar] = true
+				}
+				if stringChar == strings.ToLower(stringChar) {
+					keys[stringChar] = true
+				}
+			}
+
+			if mazeMap[Point{x, y}].tile == "" {
+				mazeMap[Point{x, y}] = droidTile{string(line[index]), false}
+			}
+			x++
+		}
+		y++
+	}
+
+	var listOfRoutes []Route
+	var startRoute Route
+	var shortestRouteSteps = math.MaxInt64
+	startRoute.pos = currentPos
+	startRoute.length = 0
+	startRoute.prevPoints = make(map[string]bool)
+	listOfRoutes = getRoutesFromStartRoute(startRoute, mazeMap, doors, keys, listOfRoutes)
+
+	for _, route := range listOfRoutes {
+		if route.length < shortestRouteSteps {
+			shortestRouteSteps = route.length
+		}
+	}
+	fmt.Print(shortestRouteSteps)
+}
+
+func getRoutesFromStartRoute(route Route, mazeMap map[Point]droidTile, doors map[string]bool, keys map[string]bool, listOfRoutes []Route) []Route {
+	var canGoToQueue []Point
+	var localDist int = 0
+	canGoToQueue = append(canGoToQueue, route.pos)
+	availableDirs := []int64{NORTH, SOUTH, WEST, EAST}
+
+	for len(canGoToQueue) != 0 {
+		var currentQueue = make([]Point, len(canGoToQueue))
+		copy(currentQueue, canGoToQueue)
+		canGoToQueue = []Point{}
+		for _, point := range currentQueue {
+			for _, dir := range availableDirs {
+				dirPoint := getPointForDirection(dir, point)
+				dirTile := mazeMap[dirPoint]
+				localDist++
+
+				if doors[dirTile.tile] {
+					//Check if we have the key
+					if !route.prevPoints[strings.ToLower(dirTile.tile)] {
+						continue
+					}
+				}
+
+				if keys[dirTile.tile] {
+					if route.prevPoints[dirTile.tile] == false {
+						route.length += localDist
+						route.pos = point
+						newListOfRoutes := getRoutesFromStartRoute(Route{route.pos, route.length, route.prevPoints}, mazeMap, doors, keys, listOfRoutes)
+						listOfRoutes = append(listOfRoutes, newListOfRoutes...)
+					}
+					route.prevPoints[dirTile.tile] = true
+					fmt.Println("found key", dirTile.tile)
+					continue
+				}
+
+				if dirTile.tile != "#" && dirTile.tile != "" && !dirTile.traveled {
+					dirTile.traveled = true
+					fmt.Println("doing thing", dirPoint)
+					mazeMap[dirPoint] = dirTile
+					canGoToQueue = append(canGoToQueue, dirPoint)
+				}
+			}
+		}
+	}
+	fmt.Println("finished route", route)
+	listOfRoutes = append(listOfRoutes, route)
+	return listOfRoutes
+}
+
+func tractorBeamDroneIOHandlers(x, y int64) (inputHandler, outputHandler, func() bool) {
+	var sentX bool = false
+	var pulled = false
+	in := func() int64 {
+		if !sentX {
+			sentX = true
+			return x
+		}
+		return y
+	}
+	out := func(output int64) {
+		if output == 1 {
+			pulled = true
+		}
+	}
+
+	getPulled := func() bool {
+		return pulled
+	}
+	return in, out, getPulled
+}
+
+func day19() {
+	var tractorBeamDroneProgramArr = []int64{109, 424, 203, 1, 21101, 11, 0, 0, 1105, 1, 282, 21102, 18, 1, 0, 1105, 1, 259, 2102, 1, 1, 221, 203, 1, 21102, 1, 31, 0, 1106, 0, 282, 21101, 38, 0, 0, 1105, 1, 259, 21001, 23, 0, 2, 21201, 1, 0, 3, 21101, 0, 1, 1, 21101, 0, 57, 0, 1105, 1, 303, 1201, 1, 0, 222, 20102, 1, 221, 3, 20101, 0, 221, 2, 21101, 259, 0, 1, 21102, 80, 1, 0, 1106, 0, 225, 21101, 127, 0, 2, 21102, 91, 1, 0, 1106, 0, 303, 1201, 1, 0, 223, 20102, 1, 222, 4, 21101, 259, 0, 3, 21101, 0, 225, 2, 21102, 225, 1, 1, 21102, 1, 118, 0, 1106, 0, 225, 21001, 222, 0, 3, 21101, 0, 89, 2, 21101, 133, 0, 0, 1105, 1, 303, 21202, 1, -1, 1, 22001, 223, 1, 1, 21101, 0, 148, 0, 1105, 1, 259, 2102, 1, 1, 223, 21002, 221, 1, 4, 21001, 222, 0, 3, 21101, 0, 21, 2, 1001, 132, -2, 224, 1002, 224, 2, 224, 1001, 224, 3, 224, 1002, 132, -1, 132, 1, 224, 132, 224, 21001, 224, 1, 1, 21102, 195, 1, 0, 106, 0, 108, 20207, 1, 223, 2, 20102, 1, 23, 1, 21102, 1, -1, 3, 21101, 0, 214, 0, 1105, 1, 303, 22101, 1, 1, 1, 204, 1, 99, 0, 0, 0, 0, 109, 5, 1201, -4, 0, 249, 22102, 1, -3, 1, 21201, -2, 0, 2, 22101, 0, -1, 3, 21102, 250, 1, 0, 1105, 1, 225, 21202, 1, 1, -4, 109, -5, 2105, 1, 0, 109, 3, 22107, 0, -2, -1, 21202, -1, 2, -1, 21201, -1, -1, -1, 22202, -1, -2, -2, 109, -3, 2106, 0, 0, 109, 3, 21207, -2, 0, -1, 1206, -1, 294, 104, 0, 99, 22101, 0, -2, -2, 109, -3, 2106, 0, 0, 109, 5, 22207, -3, -4, -1, 1206, -1, 346, 22201, -4, -3, -4, 21202, -3, -1, -1, 22201, -4, -1, 2, 21202, 2, -1, -1, 22201, -4, -1, 1, 21201, -2, 0, 3, 21101, 0, 343, 0, 1106, 0, 303, 1105, 1, 415, 22207, -2, -3, -1, 1206, -1, 387, 22201, -3, -2, -3, 21202, -2, -1, -1, 22201, -3, -1, 3, 21202, 3, -1, -1, 22201, -3, -1, 2, 22101, 0, -4, 1, 21101, 384, 0, 0, 1106, 0, 303, 1105, 1, 415, 21202, -4, -1, -4, 22201, -4, -3, -4, 22202, -3, -2, -2, 22202, -2, -4, -4, 22202, -3, -2, -3, 21202, -4, -1, -2, 22201, -3, -2, 1, 21201, 1, 0, -4, 109, -5, 2105, 1, 0}
+	var tractorBeamDroneProgram = makeMapForArray(tractorBeamDroneProgramArr)
+	pointsAffected := 0
+	var x, y int64
+	var sizeX int64 = 50
+	var sizeY int64 = 50
+	var width int64 = 100
+	var height int64 = 100
+	var wantedXCoord int64
+	var wantedYCoord int64
+	for y = 0; y < height; y++ {
+		for x = 0; x < width; x++ {
+			in, out, getPulled := tractorBeamDroneIOHandlers(x, y)
+			intComp(tractorBeamDroneProgram, in, out)
+			isPulled := getPulled()
+			if isPulled {
+				fmt.Print("#")
+				pointsAffected++
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+	fmt.Println("Points affected in 50x50 square:", pointsAffected)
+	var gottenXYClosestCoords bool = false
+	for !gottenXYClosestCoords {
+
+		//Subtract 1 because the coordinates start at 0
+		in, out, getPulled := tractorBeamDroneIOHandlers(x, y+sizeY-1)
+		intComp(tractorBeamDroneProgram, in, out)
+		isPulledBottomLeft := getPulled()
+
+		in, out, getPulled = tractorBeamDroneIOHandlers(x+sizeX-1, y)
+		intComp(tractorBeamDroneProgram, in, out)
+		isPulledTopRight := getPulled()
+
+		if isPulledBottomLeft && isPulledTopRight {
+			wantedXCoord = x
+			wantedYCoord = y
+			gottenXYClosestCoords = true
+		}
+
+		if isPulledBottomLeft {
+			x -= sizeX + 1
+			y++
+		} else {
+			x++
+		}
+	}
+	fmt.Println("Wanted coords to fit size of 100x100:", (wantedXCoord*10000)+wantedYCoord)
 }
 
 var asteroidsData = `.#......##.#..#.......#####...#..
